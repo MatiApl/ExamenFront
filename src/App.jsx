6cee4653edb07e630f43556252624d6aba84a08f
MatiApl <matiaplanalp10@gmail.com>
2022-11-08 13:39:45 -0300

    Examen

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Card from "./components/Card";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Carga de Estudiantes</h1>
+    <form></form>
+    <Card/>
+    </div>
+  );
+}
+a
+export default App;
